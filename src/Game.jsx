2c77f4c0ebb6adef14a143253bd44ac878261392
Rafael <98551688+etzoider@users.noreply.github.com>
2023-09-02 01:20:24 -0300

    Feat: backgrouround audiotrack and toggle button

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './Game.css'
 import { Menu } from './Menu'
 import { Board } from './Board'
@@ -6,12 +6,44 @@ import { ToggleIcon } from './ToggleIcon'
 
 
 function Game() {
-  const [music, setMusic] = useState(true);
+  let currentTime = 0;
+  const [music, setMusic] = useState(false);
   const [sound, setSound] = useState(true);
   const [ state, setState ] = useState({
     difficulty: 'medium',
     gameStage: 0    
-  })
+  }) 
+  
+  const playAudio = (id) => {
+    const audio = document.getElementById(id);
+    if (id == 'click') {
+      audio.volume = 0.4;
+      audio.currentTime= 0.51;
+    }
+    audio.play();
+  }
+  const pauseAudio = (id) => {
+    const audio = document.getElementById(id);
+    audio.pause();  
+  }
+
+  useEffect( () => {
+    const clickable = [document.getElementsByTagName('div')];
+
+    console.log(clickable);
+    
+    
+
+
+  }, [])
+
+  useEffect( () => {
+    if (music) {
+      playAudio('bg-audio');
+    } else {
+      pauseAudio('bg-audio');
+    }
+  }, [music])
 
   const creatures = [
 
@@ -28,9 +60,17 @@ function Game() {
 
     </div>
 
-    <ToggleIcon url={`music-${music == true ? 'on' : 'off'}.svg`} onClickFunction={() => setMusic(prev => !prev)} bgColor={'red'} />
+    <ToggleIcon url={`music-${music == true ? 'on' : 'off'}.svg`} onClickFunction={() => setMusic(prev => !prev)} offSetX={0}/>
+    <ToggleIcon url={`sound-${sound == true ? 'on' : 'off'}.svg`} onClickFunction={() => setSound(prev => !prev)} offSetX={5}/>
+
+    <button style={ {position: 'absolute', top:'30%', left:'20%', width: '50px', height: '50px', backgroundColor: 'orange'} } 
+    onClick= {() => playAudio('click')} >play</button>
+    <button style={ {position: 'absolute', top:'40%', left:'20%', width: '50px', height: '50px', backgroundColor: 'orange'} } 
+    onClick= {() => pauseAudio('bg-audio')} >stop</button>
+
     
     </>
+    
   )
 }
 
--- src/ToggleIcon.jsx
+++ src/ToggleIcon.jsx
@@ -1,10 +1,11 @@
 
 
-export const ToggleIcon = ( {url, onClickFunction, bgColor} ) => {
+export const ToggleIcon = ( {url, onClickFunction, bgColor, offSetX} ) => {
+    let leftPosition = (5 + offSetX);
     return (
-        <div className="toggle-icon-wrapper" >
+        <div className="toggle-icon-wrapper" style={ {left: `${leftPosition}%`} }>
 
-            <img className='toggle-icon' src={`${url}`} alt={`${url.slice(1)} icon`} onClick={onClickFunction} />
+            <img className='toggle-icon' src={`${url}`} alt={`${url} icon`} onClick={onClickFunction} />
 
         </div>
     )
